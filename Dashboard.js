9434552646c2dc6b1c41ed5182a9e647eb1de213
iki1uc <entertrainer@web.de>
2026-07-30 02:04:52 +0200

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,9 @@
+export function IS_DASHBOARD(is){
+    return {
+        mode: is.mode,
+        amplitude: is.amplitude,
+        spread: is.spread,
+        vector: is.vector,
+        state: is.state
+    };
+}
